59f34216642e82eec7936c901c2426ec66f426c8
JanaClaessens <janaclaessens@icloud.com>
2022-12-04 17:39:01 +0100

    fix: cors error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const messageRouter = require("./routes/message");
 const indexRouter = require("./routes/index");
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
 const mongodbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/dev5lab5'
@@ -12,6 +13,7 @@ app.set('view engine', 'pug')
 app.set('views', __dirname + '/views');
 // add json body parser
 app.use(express.json());
+app.use(cors())
 app.use("/api/v1/messages", messageRouter);
 app.use("/", indexRouter)
 
