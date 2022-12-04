bd44af625762ef2b3fd898c2bb1ec05804943a42
JanaClaessens <janaclaessens@icloud.com>
2022-12-04 17:14:42 +0100

    feat: Get port from vercel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const messageRouter = require("./routes/message");
 const indexRouter = require("./routes/index");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/dev5lab5');
